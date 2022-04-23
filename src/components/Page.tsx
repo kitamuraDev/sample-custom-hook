6824e009403b69aa6525858fbb8fb4a24fd0f03e
kitamuraDev <masato1999824@icloud.com>
2022-04-24 00:34:39 +0900

    :sparkles: v2

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,64 +1,33 @@
 /* eslint-disable react/jsx-handler-names */
-import { useState, VFC } from 'react';
+import { VFC } from 'react';
+
+import useLocalHistory from './useLocalHistory';
 
 const Page: VFC = () => {
   const toppage = 1;
   const lastpage = 4;
-  const initHistory: number[] = [toppage];
-  const [history, setHistory] = useState<number[]>(initHistory);
 
-  const currentPage = history[history.length - 1];
+  const { currentPage, top, next, back, last, reset } = useLocalHistory(
+    toppage,
+    lastpage,
+  );
 
   return (
     <div style={{ textAlign: 'center', marginTop: '300px' }}>
-      <div>現在のページ: {currentPage}</div>
-      <button
-        type='button'
-        onClick={() => {
-          if (currentPage === toppage) return;
-          const nextHistory = [...history, toppage];
-          setHistory(nextHistory);
-        }}
-      >
+      <div style={{ marginBottom: '24px' }}>現在のページ: {currentPage}</div>
+      <button type='button' onClick={() => top()}>
         トップ
       </button>
-      <button
-        type='button'
-        onClick={() => {
-          const nextpage = currentPage + 1;
-          if (lastpage < nextpage) return;
-          const nextHistory = [...history, nextpage];
-          setHistory(nextHistory);
-        }}
-      >
+      <button type='button' onClick={() => next()}>
         次へ
       </button>
-      <button
-        type='button'
-        onClick={() => {
-          if (history.length <= 1) return;
-          const nextHistory = [...history.slice(0, history.length - 1)];
-          setHistory(nextHistory);
-        }}
-      >
+      <button type='button' onClick={() => back()}>
         戻る
       </button>
-      <button
-        type='button'
-        onClick={() => {
-          if (currentPage === lastpage) return;
-          const nextHistory = [...history, lastpage];
-          setHistory(nextHistory);
-        }}
-      >
+      <button type='button' onClick={() => last()}>
         ラスト
       </button>
-      <button
-        type='button'
-        onClick={() => {
-          setHistory(initHistory);
-        }}
-      >
+      <button type='button' onClick={() => reset()}>
         リセット
       </button>
     </div>
---
+++ src/components/useLocalHistory.ts
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+const useLocalHistory = (toppage: number, lastpage: number) => {
+  const initHistory: number[] = [toppage];
+  const [history, setHistory] = useState<number[]>(initHistory);
+
+  const currentPage = history[history.length - 1];
+
+  const top = () => {
+    if (currentPage === toppage) return;
+    const nextHistory = [...history, toppage];
+    setHistory(nextHistory);
+  };
+
+  const next = () => {
+    const nextpage = currentPage + 1;
+    if (lastpage < nextpage) return;
+    const nextHistory = [...history, nextpage];
+    setHistory(nextHistory);
+  };
+
+  const back = () => {
+    if (history.length <= 1) return;
+    const nextHistory = [...history.slice(0, history.length - 1)];
+    setHistory(nextHistory);
+  };
+
+  const last = () => {
+    if (currentPage === lastpage) return;
+    const nextHistory = [...history, lastpage];
+    setHistory(nextHistory);
+  };
+
+  const reset = () => setHistory(initHistory);
+
+  return { currentPage, top, next, back, last, reset };
+};
+
+export default useLocalHistory;
